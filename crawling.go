cdc55fe43f1a07cc9cb3de90193b4699ce1918d8
Saketh <saketh.medicherla@gmail.com>
2025-10-09 00:13:23 -0400

    Recursively crawl sites

--- crawling.go
+++ crawling.go
@@ -41,7 +41,49 @@ func getHTML(rawURL string) (string, error) {
 }
 
 func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return
+	}
+	currURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		return
+	}
 
+	baseHost := baseURL.Hostname()
+	currHost := currURL.Hostname()
+
+	if !strings.EqualFold(baseHost, currHost) {
+		return 
+	}
+
+	normRaw, err := normalizeUrl(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error normalizing URL: %s\n", rawCurrentURL)
+		return
+	}
+	if val, ok := pages[normRaw]; ok {
+		pages[normRaw] += 1
+		return
+	} else {
+		pages[normRaw] = 1
+		fmt.Printf("Fetching html from: %s\n", normRaw)
+
+		currHTML, err := getHTML(normRaw)
+		if err != nil {
+			fmt.Printf("Error fetching HTML from URL: %s\n", normRaw)
+			return
+		}
+
+		currPageURLs, err := getURLsFromHTML(currHTML)
+		if err != nil {
+			fmt.Printf("Error fetching links from current page: %s\n", normRaw)
+			return
+		}
+		for _, url := range currPageURLs {
+			crawlPage(rawBaseURL, url, pages)
+		}
+	}
 }
 
 
